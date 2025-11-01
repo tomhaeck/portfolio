199e3457ee34cf50efa47d032dd79cd7beb33d0f
Tom <tomhaeck0@gmail.com>
2025-11-01 21:18:17 +0100

    url, view and template for project detail

--- projects/urls.py
+++ projects/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from projects import views
 urlpatterns = [
     path("", views.all_projects, name="all_projects"),
+    path("<int:id>/", views.project_detail, name="project_detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from projects.models import Project
 
@@ -6,3 +6,8 @@ from projects.models import Project
 def all_projects(request):
     projects = Project.objects.all()
     return render(request, "projects/all_projects.html", {"projects":projects})
+
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+    return render(request, "projects/project_detail.html", {"project":project})
