93b0ce6d5f48df78747824eae2dd79043546691c
Tom <tomhaeck0@gmail.com>
2025-11-01 21:43:32 +0100

    config for uploaded media

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from projects import views
-urlpatterns = [
+
+urlpatterns = ([
     path("", views.all_projects, name="all_projects"),
     path("<int:id>/", views.project_detail, name="project_detail"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
