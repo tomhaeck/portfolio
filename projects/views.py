1a03fa2113712f7ad8624b5f9d3c08f62aee50bb
Tom <tomhaeck0@gmail.com>
2025-11-21 21:35:46 +0100

    add filter to the view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from projects.models import Project
@@ -5,6 +6,9 @@ from projects.models import Project
 # Create your views here.
 def all_projects(request):
     projects = Project.objects.all()
+    if contains:=request.GET.get("contains"):
+        projects = projects.filter(Q(title__icontains=contains) | Q(description__icontains=contains))
+
     return render(request, "projects/all_projects.html", {"projects":projects})
 
 
